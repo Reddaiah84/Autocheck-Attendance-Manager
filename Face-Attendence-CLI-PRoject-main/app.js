eb32bcbb2ad0b0fe07f9606c44c788d244c1ee2c
Reddaiah84 <gudireddaiah20032gmail.com>
2024-07-07 18:37:28 +0530

    first commit

---
+++ Face-Attendence-CLI-PRoject-main/app.js
@@ -0,0 +1,41 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
+
+// Load models from face-api.js
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+]).then(startVideo);
+
+// Start video stream
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+  }, 100);
+});
+
+// Capture button event
+captureButton.addEventListener('click', function() {
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  // Here you can add the logic to send the captured image to the backend for processing and attendance logging
+});
